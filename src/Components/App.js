30f9a9a4ff246816a3d3c83177265e27f80de4f7
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-11-20 17:54:48 +0700

    Create Laning-component and import images

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import '../Assets/App.css';
+import Landing from './Landing';
 import Carousel from './Carousel';
 
 class App extends Component {
   render() {
     return (
       <div className="App">  
-
+        <Landing></Landing>
         <Carousel></Carousel>
       
       </div>
