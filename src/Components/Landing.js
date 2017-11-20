112372aa1ee5743f6a84327201b1232bc90ab096
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-11-20 19:39:47 +0700

    Add font

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -4,7 +4,9 @@ class Landing extends Component {
   render() {
     return (
       <div className="Landing">  
-            
+            <img src="/images/logo.png" className="logo" alt=""/>
+            <h1 className="font-200 font-3rem">ประกาศผู้มีสิทธิ์เข้าสัมภาษณ์</h1>
+            <h2 className="font-2rem">โครงการ Young Webmaster Camp 15</h2>
       </div>
     );
   }
