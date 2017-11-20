605e6e69b7468baa5f71f4e3eb0d94d7c923fc24
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-11-20 15:25:26 +0700

    Add button

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 
 class Carousel extends Component {
-  render() {
-    return (
-      <div id="carousel">
-        
-		<div className="carousel-item crs-first">A</div>
-		<div className="carousel-item crs-second">B</div>
-		<div className="carousel-item crs-third">C</div>
-		<div className="carousel-item crs-fourth">D</div>
-		<div className="carousel-item crs-fifth">E</div>
-
-      </div>
-    );
+	constructor(props) {
+	    super(props);
+	    this.state = {};
+	}
+	up = function(e){
+		e.preventDefault();
+		document.getElementById("crsFirst").style.transform = "rotate(117deg)";
+	}
+	down = function(e){
+		e.preventDefault();
+		alert('down');
+	}
+	render() {
+		return (
+			<div>
+				<div id="carousel">
+					<div className="carousel-item crs-first" id="crsFirst">A</div>
+					<div className="carousel-item crs-second" id="crsSecond">B</div>
+					<div className="carousel-item crs-third" id="crsThird">C</div>
+					<div className="carousel-item crs-fourth" id="crsFourth">D</div>
+					<div className="carousel-item crs-fifth" id="crsFifth">E</div>
+				</div>
+				<button onClick={this.up}>Up</button>
+				<button onClick={this.down}>Down</button>
+			</div>
+		);
   }
 }
 
