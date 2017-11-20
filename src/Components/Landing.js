dc4e6788cbc84cfdea0c3d472d907e5c5ccf954a
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-11-20 19:50:18 +0700

    Add reactStrap

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 
 class Landing extends Component {
   render() {
@@ -7,6 +8,7 @@ class Landing extends Component {
             <img src="/images/logo.png" className="logo" alt=""/>
             <h1 className="font-200 font-3rem">ประกาศผู้มีสิทธิ์เข้าสัมภาษณ์</h1>
             <h2 className="font-2rem">โครงการ Young Webmaster Camp 15</h2>
+            <Button color="primary">asdasd</Button>
       </div>
     );
   }
