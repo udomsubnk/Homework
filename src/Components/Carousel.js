e212b7ebc2901a39b0829eaa581c934ac4f738de
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-11-20 16:22:27 +0700

    Can Scroll down/up the carousel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -3,26 +3,72 @@ import data from '../Assets/data';
 
 class Carousel extends Component {
 	componentWillMount(){
-
+		this.setState((prevState) => {});
 	}
 	constructor(props) {
 	    super(props);
 	    console.log(data);
 	    this.state = {
-	    	crsFirst: "",
-	    	crsSecond: "",
-	    	crsThird: data[0].firstName,
-	    	crsFourth: data[1].firstName,
-	    	crsFifth: data[2].firstName
+	    	count: 0,
+				crsFirst: "",
+				crsSecond: "",
 	    };
+	    this.state.crsThird = data[this.state.count].firstName;
+	    this.state.crsFourth = data[this.state.count+1].firstName;
+	    this.state.crsFifth = data[this.state.count+2].firstName;
+
+	    this.up = this.up.bind(this);
+	    this.down = this.down.bind(this);
 	}
 	up = function(e){
 		e.preventDefault();
-		
+		this.setState((prevState) => {
+			if( this.state.count -3 >= 0 ){
+				return {
+						count: prevState.count - 1,
+				  	crsFifth: prevState.crsFourth,
+				  	crsFourth: prevState.crsThird,
+				  	crsThird: prevState.crsSecond,
+				  	crsSecond: prevState.crsFirst,
+				  	crsFirst: data[this.state.count-2].firstName
+			  }
+			}else if( this.state.count -3 >= -2 ){
+				return {
+						count: prevState.count - 1,
+				  	crsFifth: prevState.crsFourth,
+				  	crsFourth: prevState.crsThird,
+				  	crsThird: prevState.crsSecond,
+				  	crsSecond: prevState.crsFirst,
+				  	crsFirst: ""
+			  }
+			}
+		});
 	}
 	down = function(e){
 		e.preventDefault();
-		alert('down');
+		if( this.state.count + 3 <= data.length ){
+			this.setState((prevState) => {
+				return {
+						count: prevState.count + 1,
+				  	crsFirst: prevState.crsSecond,
+				  	crsSecond: prevState.crsThird,
+				  	crsThird: prevState.crsFourth,
+				  	crsFourth: prevState.crsFifth,
+				  	crsFifth: data[this.state.count+2].firstName
+				  }
+			});
+		}else if( this.state.count + 3 === data.length + 1 || this.state.count + 3 === data.length + 2){
+			this.setState((prevState) => {
+				return {
+						count: prevState.count + 1,
+				  	crsFirst: prevState.crsSecond,
+				  	crsSecond: prevState.crsThird,
+				  	crsThird: prevState.crsFourth,
+				  	crsFourth: prevState.crsFifth,
+				  	crsFifth: ""
+				  }
+			});
+		}
 	}
 	render() {
 		return (
