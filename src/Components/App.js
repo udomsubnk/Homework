ae08be1f3c0588fec8680746f1b8f71d46d959d5
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-11-20 14:49:36 +0700

    Removed unuse files and create Carousel component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from '../Assets/logo.svg';
 import '../Assets/App.css';
+import Carousel from './Carousel';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        
+        <Carousel></Carousel>
+
       </div>
     );
   }
---
+++ src/Components/Carousel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Carousel extends Component {
+  render() {
+    return (
+      <div id="carousel">
+        Hello world!!
+      </div>
+    );
+  }
+}
+
+export default Carousel;
