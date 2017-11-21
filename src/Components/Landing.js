0bda3e2a658b83f9785b11f0764aca0b6b8261b9
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-11-21 21:24:23 +0700

    Create landing-content block

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -4,11 +4,14 @@ import { Button } from 'reactstrap';
 class Landing extends Component {
   render() {
     return (
-      <div className="Landing">  
-            <img src="/images/logo.png" className="logo" alt=""/>
-            <h1 className="font-200 font-3rem">ประกาศผู้มีสิทธิ์เข้าสัมภาษณ์</h1>
-            <h2 className="font-2rem">โครงการ Young Webmaster Camp 15</h2>
-            <Button color="primary">asdasd</Button>
+      <div className="Landing">
+	        <img src="/images/logo.png" className="logo" alt=""/>
+	      	<div className="landing-content content-block flexbox flex-center">
+		        <h1 className="font-200 font-3rem">ประกาศผู้มีสิทธิ์เข้าสัมภาษณ์</h1>
+		        <h2 className="font-2rem">โครงการ Young Webmaster Camp 15</h2>
+
+		        
+	        </div>
       </div>
     );
   }
