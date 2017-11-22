d3ca1e63ea08cb1a09e46acbaa25c8701e4ec6ec
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-11-22 12:21:51 +0700

    Create Deatil component and insert third Carousel's bix-shadow

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import '../Assets/App.css';
 import Landing from './Landing';
+import Detail from './Detail';
 
 class App extends Component {
   render() {
     return (
       <div className="App">  
         <Landing></Landing>
-      
+        <Detail></Detail>
       </div>
     );
   }
---
+++ src/Components/Detail.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Landing from './Landing';
+
+class Detail extends Component {
+  render() {
+    return (
+      <div className="Detail">  
+        <Landing></Landing>
+      
+      </div>
+    );
+  }
+}
+
+export default Detail;
