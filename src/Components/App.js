73e18422a10b4e91e3e86c2d5f47b45ac0e09c11
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-11-23 16:14:31 +0700

    Add jquery

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,11 +4,6 @@ import Landing from './Landing';
 import Detail from './Detail';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-  componentWillMount(){
-  }
   render() {
     return (
       <div className="App">  
