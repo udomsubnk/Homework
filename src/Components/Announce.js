1c47aaf2a26ab49b714ff9f0f7be52a33aed020c
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-11-24 11:38:04 +0700

    Announce on table

--- src/Components/Announce.js
+++ src/Components/Announce.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import fakeData from '../Assets/data';
+import _ from 'lodash';
 
 class Detail extends Component {
   Data = fakeData;
@@ -8,28 +9,32 @@ class Detail extends Component {
     axios.get('https://ywc15.ywc.in.th/api/interview')
     .then( response => {
         this.Data = response.data;
-        this.Data.unshift('')
-        this.Data.unshift('')
-        this.Data.push('')
-        this.Data.push('')
+        this.Data = _.sortBy(this.Data, [function(o) { return o.interviewRef; }]);
+        this.Data.unshift('','')
+        this.Data.push('','')
         this.forceUpdate();
     })
   }
-  rows = [];
-  for( i=0; i < this.Data; i++){
-    rows.push(
-      <tr>
-        <th>{this.Data[i].interviewRef}</th>
-        <th>{this.Data[i].firstName}</th>
-        <th>{this.Data[i].lastName}</th>
-        <th>{this.Data[i].major}</th>
-      </tr>
-    );
-  }
   render() {
+    var rows = [];
+    for(var i=0; i < this.Data.length; i++){
+      rows.push(
+        <tr key={this.Data[i].interviewRef}>
+          <th>{this.Data[i].interviewRef}</th>
+          <th>{this.Data[i].firstName}</th>
+          <th>{this.Data[i].lastName}</th>
+          <th>{this.Data[i].major}</th>
+        </tr>
+      );
+    }
+    console.log(rows)
     return (
       <div>
-        
+        <table>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
       </div>
     );
   }
