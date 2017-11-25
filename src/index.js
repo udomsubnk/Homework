d221d4ed9e8a85b141227e9e99609b2ec8f38615
Udomsub Naikorn <udomsub.nk@gmail.com>
2017-11-25 15:21:45 +0700

    Import bootstrap css by index.js not webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
 import './Assets/index.css';
 import App from './Components/App';
 import registerServiceWorker from './registerServiceWorker';
